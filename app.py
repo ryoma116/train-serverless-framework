f955c52a42551d062bcf5a236c1f52e7dbd84f42
ryoma116 <uma19880116>
2021-07-01 04:13:24 +0900

    :sparkles: feat: Add local boot configuration

--- app.py
+++ app.py
@@ -9,11 +9,20 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 USERS_TABLE = os.environ['USERS_TABLE']
-client = boto3.client('dynamodb')
+IS_OFFLINE = os.environ.get('IS_OFFLINE')
+
+if IS_OFFLINE:
+    client = boto3.client(
+        'dynamodb',
+        region_name='localhost',
+        endpoint_url='http://localhost:5555'
+    )
+else:
+    client = boto3.client('dynamodb')
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return jsonify(os.environ)
 
 
 @app.route("/users/<string:user_id>")
